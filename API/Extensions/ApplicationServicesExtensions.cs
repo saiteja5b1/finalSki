5b1f8f7538930033cf8894fcd3cc1efd27e87595
sai teja <58000385+saiteja5b1@users.noreply.github.com>
2021-06-23 20:27:56 +0530

    added redis for basket purpose

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -11,6 +11,7 @@ namespace API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services){
             services.AddScoped<IProductRepository,ProductRepository>(); 
+            services.AddScoped<IBasketRepository,BasketRepository>();
             services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
             services.Configure<ApiBehaviorOptions>(options => {
                 options.InvalidModelStateResponseFactory = actionResult => {
--- API/Startup.cs
+++ API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -25,7 +26,12 @@ namespace API
             
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
             services.AddDbContext<StoreContext>(p => p.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddSingleton<IConnectionMultiplexer>(p =>{
+                var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"),true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
             services.AddCors(opt =>{
